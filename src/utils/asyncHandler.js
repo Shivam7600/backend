5113e92997daf706cc59480f8d06de7e4a07cc4d
Shivam <shivammanav36fyvc76@gmail.com>
2024-01-06 21:03:14 +0530

    util code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,32 @@
+// primises method. 
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+export { asyncHandler }
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => {() => {}}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+
+
+// // try catch method. 
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(re, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
+
+
+
+    
