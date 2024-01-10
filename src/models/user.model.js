f691942f670aa29d7750ff626f68f409f26f5932
Shivam <shivammanav36fyvc76@gmail.com>
2024-01-11 00:57:21 +0530

    add register controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,7 +2,7 @@ import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
-const Schema = new Schema(
+const userSchema = new Schema(
     {
         username: {
             type: String,
